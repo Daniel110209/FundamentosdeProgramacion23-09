fb6356f3ee4c04d16fe5dbd8e1f74e51988e6dbe
Jheimy Sifuentes <N00534090@upn.pe>
2026-09-23 09:18:27 -0500

    Calcular Promedio de las Notas

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Funcion de Imprimir notas
+static void ImprimirNotas(int[] notas)
+{
+    for(int i=0; i<notas.Length; i++)
+    {
+        Console.WriteLine(notas[i]);
+    }
+}
+
+
+int[] notas={15,18,12,17,20};
+
+//Imprimir las notas
+ImprimirNotas(notas);
