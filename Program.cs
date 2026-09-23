a934c00bf2fa74c6f1f4a0aee5945a6de17bc288
Jheimy Sifuentes <N00534090@upn.pe>
2026-09-23 10:20:06 -0500

    Imprimir las nota mayor y menor

--- Program.cs
+++ Program.cs
@@ -8,16 +8,57 @@ static void ImprimirNotas(int[] notas)
 }
 
 
+//Función Calcular promedio
+static double CalcularPromedio(int[] notas)
+{
+    int suma=0;
+    for(int i=0; i<notas.Length; i++)
+    {
+        suma+=notas[i];
+    }
+    return (double)suma / notas.Length;
+}
+
+//Calcular nota mayor
+static int CalcularNotaMayor(int[] notas)
+{
+    int mayor = notas[0];
+    for(int i=1; i<notas.Length; i++)
+    {
+        if(notas[i] > mayor)
+        {
+            mayor = notas[i];
+        }
+    }
+    return mayor;
+}
+
+//Calcular nota menor
+static int CalcularNotaMenor(int[] notas)
+{
+    int menor = notas[0];
+    for(int i=1; i<notas.Length; i++)
+    {
+        if(notas[i] < menor)
+        {
+            menor = notas[i];
+        }
+    }
+    return menor;
+} 
+
 int[] notas={15,18,12,17,20};
 
 //Imprimir las notas
 ImprimirNotas(notas);
 
 //Calcular promedio de las notas 
-int suma=0;
-for(int i=0; i<notas.Length; i++)
-{
-    suma+=notas[i];
-}
-double promedio = (double)suma / notas.Length;
+
+double promedio = CalcularPromedio(notas);
 Console.WriteLine($"El promedio es: {promedio}");
+
+// Imprimir las notas mayor y menor
+int mayor = CalcularNotaMayor(notas);
+int menor = CalcularNotaMenor(notas);
+Console.WriteLine($"La nota mayor es: {mayor}");
+Console.WriteLine($"La nota menor es: {menor}");
