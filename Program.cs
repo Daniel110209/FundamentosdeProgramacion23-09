f2e69e0d1dec0a1a9608d78fdf808247d14ace4e
Jheimy Sifuentes <N00534090@upn.pe>
2026-09-23 09:31:46 -0500

    Agregar cálculo del promedio de notas

--- Program.cs
+++ Program.cs
@@ -12,3 +12,12 @@ int[] notas={15,18,12,17,20};
 
 //Imprimir las notas
 ImprimirNotas(notas);
+
+//Calcular promedio de las notas 
+int suma=0;
+for(int i=0; i<notas.Length; i++)
+{
+    suma+=notas[i];
+}
+double promedio = (double)suma / notas.Length;
+Console.WriteLine($"El promedio es: {promedio}");
